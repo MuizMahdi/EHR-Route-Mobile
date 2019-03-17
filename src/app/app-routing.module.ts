75fe615ecb51e79506ed4ae66aca1971d7667110
MuizMahdi <Moezmahdy@gmail.com>
2019-03-17 21:28:31 +0300

    [Update] Added login and registeration components/pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './Pages/Auth/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './Pages/Auth/register/register.module#RegisterPageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+
+   declarations: [
+      AppComponent
+   ],
+
+   entryComponents: [],
+
+   imports: [
+      BrowserModule, 
+      IonicModule.forRoot({
+         rippleEffect: false
+      }), 
+      AppRoutingModule,
+   ],
+
+   providers: [
+      StatusBar,
+      SplashScreen,
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+   ],
+
+   bootstrap: [
+      AppComponent
+   ]
+
 })
+
+
 export class AppModule {}
