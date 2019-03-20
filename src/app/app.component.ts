7ef228fe8d8ad1824aa7e303cc004db64d6e4992
MuizMahdi <Moezmahdy@gmail.com>
2019-03-20 20:23:56 +0300

    [Update] Added TypeORM DBs connection creation on startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { createConnection } from 'typeorm'
 
 
 @Component({
@@ -10,19 +11,60 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 
 
-export class AppComponent {
+export class AppComponent 
+{
+
    constructor(
-      private platform: Platform,
-      private splashScreen: SplashScreen,
+      private platform: Platform, private splashScreen: SplashScreen,
       private statusBar: StatusBar
    ) {
       this.initializeApp();
+      this.initializeDatabase();
    }
 
-   initializeApp() {
+
+   initializeApp() 
+   {
       this.platform.ready().then(() => {
          this.statusBar.styleDefault();
          this.splashScreen.hide();
       });
    }
+
+
+   async initializeDatabase()
+   {
+      // Depending on the machine the app is running on,
+      // configure different database connections
+
+      // Running on device or emulator
+      if(this.platform.is('cordova')) 
+      {
+         await createConnection({
+            type: 'cordova',
+            database: 'test',
+            location: 'default',
+            logging: ['error', 'query', 'schema'],
+            synchronize: true,
+            entities: [
+
+            ]
+         });
+      }
+      else 
+      {
+         // Running in browser
+         await createConnection({
+            type: 'sqljs',
+            autoSave: true,
+            location: 'browser',
+            logging: ['error', 'query', 'schema'],
+            synchronize: true,
+            entities: [
+
+            ]
+         });
+      }
+   }
+
 }
