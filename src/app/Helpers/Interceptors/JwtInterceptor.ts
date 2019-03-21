2fac20102fa246706067f37b3a16bc827ee03eaa
MuizMahdi <Moezmahdy@gmail.com>
2019-03-21 22:27:30 +0300

    [Update] Added a Json Web Token http interceptor

---
+++ src/app/Helpers/Interceptors/JwtInterceptor.ts
@@ -0,0 +1,34 @@
+import { HttpRequest, HttpInterceptor, HttpEvent, HttpHandler } from '@angular/common/http';
+import { AuthService } from './../../Services/auth.service';
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor
+{
+   constructor(private authService: AuthService)
+   { }
+
+
+   // Adds the saved JsonWebToken (JWT) to the Authorization header of all requests
+   intercept(request: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>>
+   {
+      // Get the token from local storage
+      let Jwt = localStorage.getItem('accessToken');
+
+      // If it exists
+      if (Jwt) 
+      {
+         // Clone the request and set its authorization header with the token
+         request = request.clone({
+            setHeaders: {
+               Authorization: "Bearer " + Jwt
+            }
+         });
+      }
+
+      // Handle the request and move into next interceptor if available
+      return handler.handle(request); 
+   }
+}
