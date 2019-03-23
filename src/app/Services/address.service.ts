51bd036b00ceb5b0132f32dffc8b5987681ce796
MuizMahdi <Moezmahdy@gmail.com>
2019-03-24 00:12:41 +0300

    [Update] Added address generation on users first login

--- src/app/Services/address.service.ts
+++ src/app/Services/address.service.ts
@@ -1,9 +1,33 @@
+import { AddressResponse } from './../Models/Payload/Responses/AddressResponse';
+import { ErrorResponse } from './../Models/Payload/Responses/ErrorResponse';
+import { Toast } from '@ionic-native/toast/ngx';
+import { first, catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Observable, throwError } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AddressService {
 
-  constructor() { }
+export class AddressService 
+{
+   private addressGenerationUrl:string = environment.apiUrl + '/address/generate';
+
+
+   constructor(private http:HttpClient) 
+   { }
+
+
+   public generateUserAddress(): Observable<any>
+   {
+      return this.http.get(this.addressGenerationUrl).pipe(first(),
+         catchError(error => {
+            return throwError(error);
+         })
+      );
+   }
+
 }
